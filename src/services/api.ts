d061852a4a14f7d65430274f97bd7f8e504d4c49
[QA] Michel Mendoza <michel.mendoza@voltbras.com.br>
2024-09-07 17:46:35 -0300

    feat: code vercel

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,8 @@
-import axios from 'axios'
+import axios from 'axios';
+
+// Acessando variáveis de ambiente no Vite
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 export const api = axios.create({
-    baseURL: "http://localhost:3333/"
-})
+    baseURL: baseURL || "http://localhost:3333/"  // Fallback para localhost se a variável não estiver definida
+});
